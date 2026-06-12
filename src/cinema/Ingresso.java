c433dd247724c3eaf94a38ce22194d66b4182a27
Isabelle Sombrio <isabellesombrio@gmail.com>
2026-06-11 21:21:40 -0300

    Simplify Ingresso.java

--- src/cinema/Ingresso.java
+++ src/cinema/Ingresso.java
@@ -2,27 +2,13 @@ package cinema;
 
 /**
  * Classe Ingresso - Representa um ingresso vendido
- * 
- * Armazena todas as informações de uma venda:
- * - Localização (fila e poltrona)
- * - Tipo (inteira ou meia-entrada)
- * - Preço final
  */
 public class Ingresso {
-    
-    // Atributos privados do ingresso
     private int fila;
     private int poltrona;
-    private String tipo; // "Inteira" ou "Meia"
+    private String tipo;
     private double preco;
     
-    /**
-     * Construtor do Ingresso
-     * @param fila número da fila (0-9)
-     * @param poltrona número da poltrona (0-9)
-     * @param tipo tipo de ingresso ("Inteira" ou "Meia")
-     * @param preco preço final do ingresso
-     */
     public Ingresso(int fila, int poltrona, String tipo, double preco) {
         this.fila = fila;
         this.poltrona = poltrona;
@@ -30,42 +16,19 @@ public class Ingresso {
         this.preco = preco;
     }
     
-    // ==================== GETTERS ====================
-    
-    /**
-     * Retorna a fila do ingresso
-     */
     public int getFila() {
         return this.fila;
     }
     
-    /**
-     * Retorna a poltrona do ingresso
-     */
     public int getPoltrona() {
         return this.poltrona;
     }
     
-    /**
-     * Retorna o tipo de ingresso
-     */
     public String getTipo() {
         return this.tipo;
     }
     
-    /**
-     * Retorna o preço do ingresso
-     */
     public double getPreco() {
         return this.preco;
-    }
-    
-    /**
-     * Retorna uma representação em string do ingresso
-     */
-    @Override
-    public String toString() {
-        return "Fila " + this.fila + ", Poltrona " + this.poltrona + 
-               " - " + this.tipo + " (R$ " + String.format("%.2f", this.preco) + ")";
     }
 }
